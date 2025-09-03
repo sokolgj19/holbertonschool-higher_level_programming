203a4f6b9ee20642146376186b04b582ad6df31a
sokolgj19 <gjekasokol19@gmail.com>
2025-09-03 10:36:27 +0000

    Wrote a program that prints numbers from 0 to 99 with space and coma

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for i in range(0, 100):
+    if i < 10:
+        print("0{}".format(i), end=", " if i < 99 else "\n")
+    else:
+        print("{}".format(i), end=", " if i < 99 else "\n")
