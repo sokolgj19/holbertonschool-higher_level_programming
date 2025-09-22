cfff907baaebfb4ec829436d92741eb77590c2c0
sokolgj19 <gjekasokol19@gmail.com>
2025-09-22 21:21:46 +0000

    a class BaseGeometry with a method that validates value and raises TypeError and ValueError

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,6 +11,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         ''' method that validates value '''
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
