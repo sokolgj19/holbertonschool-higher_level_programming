0b658caefc1a5091a116be30681d5908d2c7f4e4
sokolgj19 <gjekasokol19@gmail.com>
2025-09-22 21:18:24 +0000

    a class BaseGeometry with a method that validates value and raises TypeError and ValueError

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' class with method that raises exception '''
+
+
+class BaseGeometry:
+    ''' class with method that raises exception '''
+    def area(self):
+        ''' method that raises exception '''
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        ''' method that validates value '''
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
