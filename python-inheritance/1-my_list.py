ca189e0bec4e5755bee7f157e36b4f9f4109bcfb
sokolgj19 <gjekasokol19@gmail.com>
2025-09-22 11:23:59 +0000

    a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""""a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """prints the list, but sorted """
+    def print_sorted(self):
+        print(sorted(self))
