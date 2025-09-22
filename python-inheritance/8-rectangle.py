4930bb87a2c823d61b879cb89993726388d627fe
sokolgj19 <gjekasokol19@gmail.com>
2025-09-22 21:59:48 +0000

    a class Rectangle that inherits from BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 ''' class that inherits from another class '''
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 class Rectangle(BaseGeometry):
     ''' class that inherits from another class '''
+    
     def __init__(self, width, height):
         ''' instantation with width and height '''
         self.integer_validator("width", width)
