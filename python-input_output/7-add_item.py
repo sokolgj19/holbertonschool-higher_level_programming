24d76599661b5f598b744e67975683e8db56f46d
sokolgj19 <gjekasokol19@gmail.com>
2025-09-30 12:01:37 +0000

    a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to a Python list, and then save them to a file"""
+import json
+import os.path
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+python_list = []
+if os.path.exists("add_item.json"):
+    python_list = load_from_json_file("add_item.json")
+save_to_json_file(python_list + argv[1:], "add_item.json")
