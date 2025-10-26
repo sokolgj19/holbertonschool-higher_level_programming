9bc3637f63d160b5fc9ba24177bc720f2d9317c2
sokolgj19 <gjekasokol19@gmail.com>
2025-10-26 11:52:53 +0100

    Completed all sql tasks

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Create Another table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT default 1, name VARCHAR(256));
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Create Another table
+CREATE TABLE IF NOT EXISTS unique_id (id INT default 1 UNIQUE, name VARCHAR(256));
